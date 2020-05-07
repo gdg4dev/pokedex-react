56166a53269f8cb19ee307eca36e8875c4eb97ef
Aniket Hirpara <gdg4dev@gmail.com>
2020-05-07 13:29:29 +0530

    init

---
+++ precache-manifest.ac95cb7d6e2402ad5203e7417da1c972.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b162c3cf4f47b7c50d5032a8f1d59cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0277436417b03938739e",
+    "url": "/static/css/main.b4dd493c.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "0277436417b03938739e",
+    "url": "/static/js/main.473af70e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
